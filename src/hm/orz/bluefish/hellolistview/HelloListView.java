e4d3479d7257c8c6b260291c7a81b7b493072180
aquabluefish <aquabluefish@gmail.com>
2014-09-25 18:24:55 +0900

    OnItemClickListener

--- src/hm/orz/bluefish/hellolistview/HelloListView.java
+++ src/hm/orz/bluefish/hellolistview/HelloListView.java
@@ -2,8 +2,13 @@ package hm.orz.bluefish.hellolistview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HelloListView extends Activity {
     private ListView list;
@@ -13,10 +18,20 @@ public class HelloListView extends Activity {
         super.onCreate(icicle);
         setContentView(R.layout.main);
 
-        String[] arr = {"AAA","BBB","CCC","DDD","EEE","FFF"};
+        String[] ldata = {"AAA","BBB","CCC","DDD","EEE","FFF"};
 
         list = (ListView)this.findViewById(R.id.listView1);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list, arr);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list, ldata);
         list.setAdapter(adapter);
+        list.setSelection(1);
+
+        list.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?>adapter, View view, int position, long id) {
+	            TextView tview = (TextView)view;
+                Toast.makeText(HelloListView.this, tview.getText() +" が選択されました", Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
 }
